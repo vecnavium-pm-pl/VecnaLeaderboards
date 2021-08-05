9e4c2bf5029e1bc1dbcf01de0d33a9749c651e5c
Atlqs <ale3x90@gmail.com>
2021-08-05 14:54:57 +0200

    Money Leaderboard added
    uwu

--- src/Vecnavium/VecnaLeaderboards/Leaderboard/UpdateLeaderboardsTask.php
+++ src/Vecnavium/VecnaLeaderboards/Leaderboard/UpdateLeaderboardsTask.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 namespace Vecnavium\VecnaLeaderboards\Leaderboard;
 
 use pocketmine\scheduler\Task;
+use Vecnavium\VecnaLeaderboards\Main;
 
 /**
  * Class UpdateLeaderboardsTask
+ * @property string playerName
+ * @property Main plugin
  * @package Vecnavium\VecnaLeaderboards\Leaderboard
  */
 class UpdateLeaderboardsTask extends Task
@@ -14,12 +17,14 @@ class UpdateLeaderboardsTask extends Task
 	/** @var LeaderboardManager */
 	private $manager;
 
-	/**
-	 * UpdateLeaderboardsTask constructor.
-	 * @param LeaderboardManager $manager
-	 */
-	public function __construct(LeaderboardManager $manager)
+    /**
+     * UpdateLeaderboardsTask constructor.
+     * @param LeaderboardManager $manager
+     * @param Main $plugin
+     */
+	public function __construct(LeaderboardManager $manager, Main $plugin)
 	{
+        $this->plugin = $plugin;
 		$this->manager = $manager;
 	}
 
--- src/Vecnavium/VecnaLeaderboards/Main.php
+++ src/Vecnavium/VecnaLeaderboards/Main.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Vecnavium\VecnaLeaderboards;
 
+use onebone\economyapi\EconomyAPI;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityLevelChangeEvent;
@@ -12,11 +13,12 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 use Vecnavium\VecnaLeaderboards\Commands\LeaderboardCommand;
 use Vecnavium\VecnaLeaderboards\Leaderboard\LeaderboardManager;
+use Vecnavium\VecnaLeaderboards\Leaderboard\UpdateMoney;
 use Vecnavium\VecnaLeaderboards\Provider\UserDataSessionProvider;
 use Vecnavium\VecnaLeaderboards\Provider\YamlDataProvider;
-use onebone\economyapi\EconomyAPI;
 
 /**
  * Class Main
@@ -25,44 +27,48 @@ use onebone\economyapi\EconomyAPI;
 class Main extends PluginBase implements Listener
 {
 
-	public const LEADERBOARD_TYPE_KILLS = "kills";
-	public const LEADERBOARD_TYPE_STREAKS = "streaks";
-	public const LEADERBOARD_TYPE_DEATHS = "deaths";
-	public const LEADERBOARD_TYPE_LEVELS = "levels";
+    public const LEADERBOARD_TYPE_KILLS = "kills";
+    public const LEADERBOARD_TYPE_STREAKS = "streaks";
+    public const LEADERBOARD_TYPE_DEATHS = "deaths";
+    public const LEADERBOARD_TYPE_LEVELS = "levels";
     public const LEADERBOARD_TYPE_KDR = "kdr";
     public const LEADERBOARD_TYPE_MONEY = "money";
 
-	/** @var Main */
-	private static $instance;
-	/** @var YamlDataProvider */
-	private $yamlProvider;
-	/** @var LeaderboardManager */
-	private $leaderboardManager;
-	/** @var UserDataSessionProvider[] */
-	private $sessions = []
+    /** @var Main */
+    private static $instance;
+    /** @var YamlDataProvider */
+    private $yamlProvider;
+    /** @var LeaderboardManager */
+    private $leaderboardManager;
+    /** @var UserDataSessionProvider[] */
+    private $sessions = [];
 
-        public function onLoad()
-        {
-            UpdateNotifier::checkUpdate($this->getDescription()->getName(), $this->getDescription()->getVersion());
+    public function onLoad() {
+//            UpdateNotifier::checkUpdate($this->getDescription()->getName(), $this->getDescription()->getVersion());
+        /* ^^^ DOES NOT WORK ^^^ */
+    }
+
+
+    public function onEnable(): void {
+        self::$instance = $this;
+        $this->yamlProvider = new YamlDataProvider($this);
+        $this->leaderboardManager = new LeaderboardManager($this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getCommandMap()->register("VecnaLeaderboards", new LeaderboardCommand($this));
+        if($this->getConfig()->get('topmoney-leaderboard') == 'true'){
+            if($this->getServer()->getPluginManager()->getPlugin('EconomyAPI') !== null){
+                $this->getServer()->getPluginManager()->registerEvents(new UpdateMoney(), $this);
+            }
         }
-
-
-	public function onEnable(): void
-	{
-		self::$instance = $this;
-		$this->yamlProvider = new YamlDataProvider($this);
-		$this->leaderboardManager = new LeaderboardManager($this);
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getServer()->getCommandMap()->register("VecnaLeaderboards", new LeaderboardCommand($this));
         $this->checkEconomyPlugin();
-	}
+    }
 
-	public function onDisable(): void
-	{
-		$this->leaderboardManager->saveLeaderboards();
-	}
+    public function onDisable(): void
+    {
+        $this->leaderboardManager->saveLeaderboards();
+    }
 
-    public function checkEconomyPlugin(){
+    public function checkEconomyPlugin() {
         if($this->getConfig()->get('topmoney-leaderboard') == 'true'){
             if($this->getServer()->getPluginManager()->getPlugin('EconomyAPI') === null){
                 $this->getLogger()->warning('§cEconomyAPI Plugin not found. Please install EconomyAPI.');
@@ -72,115 +78,127 @@ class Main extends PluginBase implements Listener
         }
     }
 
-	/**
-	 * @param string $option
-	 * @return bool
-	 */
-	public static function isValidLeaderboard(string $option): bool
-	{
-		$options = [
-			self::LEADERBOARD_TYPE_KILLS, self::LEADERBOARD_TYPE_STREAKS,
-			self::LEADERBOARD_TYPE_DEATHS, self::LEADERBOARD_TYPE_LEVELS, self::LEADERBOARD_TYPE_KDR
-		];
-		return in_array($option, $options);
-	}
+    /**
+     * @param string $option
+     * @return bool
+     */
+    public static function isValidLeaderboard(string $option): bool
+    {
+        $options = [
+            self::LEADERBOARD_TYPE_KILLS, self::LEADERBOARD_TYPE_STREAKS,
+            self::LEADERBOARD_TYPE_DEATHS, self::LEADERBOARD_TYPE_LEVELS,
+            self::LEADERBOARD_TYPE_KDR, self::LEADERBOARD_TYPE_MONEY
+        ];
+        return in_array($option, $options);
+    }
 
-	/**
-	 * @param Player $player
-	 * @return UserDataSessionProvider|null
-	 */
-	public function getSessionFor(Player $player): ?UserDataSessionProvider
-	{
-		return $this->sessions[$player->getName()] ?? null;
-	}
+    /**
+     * @param Player $player
+     * @return UserDataSessionProvider|null
+     */
+    public function getSessionFor(Player $player): ?UserDataSessionProvider
+    {
+        return $this->sessions[$player->getName()] ?? null;
+    }
 
-	/**
-	 * @param PlayerJoinEvent $event
-	 * @priority NORMAL
-	 */
-	public function onPlayerJoin(PlayerJoinEvent $event): void
-	{
-		$player = $event->getPlayer();
-		$this->sessions[$player->getName()] = new UserDataSessionProvider($player);
-		$this->leaderboardManager->handleLeaderboardSpawning($player, $player->getLevel());
-	}
+    /**
+     * @param PlayerJoinEvent $event
+     * @priority NORMAL
+     */
+    public function onPlayerJoin(PlayerJoinEvent $event): void
+    {
+        $player = $event->getPlayer();
+        $this->sessions[$player->getName()] = new UserDataSessionProvider($player);
+        $this->leaderboardManager->handleLeaderboardSpawning($player, $player->getLevel());
+        $name = $event->getPlayer()->getName();
+        $config = new Config(Main::getInstance()->getDataFolder() . "data/$name.yml");
+        if($config->get("money") !== EconomyAPI::getInstance()->myMoney($name)) {
+            $config->set("money", EconomyAPI::getInstance()->myMoney($name));
+            $config->save();
+        } else {
+            if($config->get("money") === null) {
+                $config->set("money", EconomyAPI::getInstance()->myMoney($name));
+                $config->save();
+            }
+        }
+    }
 
-	/**
-	 * @param EntityLevelChangeEvent $event
-	 * @priority NORMAL
-	 */
-	public function onEntityLevelChangeEvent(EntityLevelChangeEvent $event): void
-	{
-		$target = $event->getTarget();
-		$origin = $event->getOrigin();
-		$player = $event->getEntity();
-		if (!$player instanceof Player) {
-			return;
-		}
-		$this->leaderboardManager->handleLeaderboardSpawning($player, $target, $origin);
-	}
+    /**
+     * @param EntityLevelChangeEvent $event
+     * @priority NORMAL
+     */
+    public function onEntityLevelChangeEvent(EntityLevelChangeEvent $event): void
+    {
+        $target = $event->getTarget();
+        $origin = $event->getOrigin();
+        $player = $event->getEntity();
+        if (!$player instanceof Player) {
+            return;
+        }
+        $this->leaderboardManager->handleLeaderboardSpawning($player, $target, $origin);
+    }
 
-	/**
-	 * @param EntityDamageEvent $event
-	 * @priority NORMAL
-	 */
-	public function onEntityDamage(EntityDamageEvent $event): void
-	{
-		$victim = $event->getEntity();
-		if (!$victim instanceof Player) {
-			return;
-		}
-		if ($event instanceof EntityDamageByEntityEvent) {
-			$damager = $event->getDamager();
-			if (!$damager instanceof Player) {
-				return;
-			}
-			if ($event->getFinalDamage() > $victim->getHealth()) {
-				$damagerSession = $this->getSessionFor($damager);
-				$victimSession = $this->getSessionFor($victim);
-				$damagerSession->addKill();
-				$victimSession->addDeath($damager);
-			}
-			return;
-		}
-		if ($event->getFinalDamage() > $victim->getHealth()) {
-			$session = $this->getSessionFor($victim);
-			$session->addDeath();
-		}
-	}
+    /**
+     * @param EntityDamageEvent $event
+     * @priority NORMAL
+     */
+    public function onEntityDamage(EntityDamageEvent $event): void
+    {
+        $victim = $event->getEntity();
+        if (!$victim instanceof Player) {
+            return;
+        }
+        if ($event instanceof EntityDamageByEntityEvent) {
+            $damager = $event->getDamager();
+            if (!$damager instanceof Player) {
+                return;
+            }
+            if ($event->getFinalDamage() > $victim->getHealth()) {
+                $damagerSession = $this->getSessionFor($damager);
+                $victimSession = $this->getSessionFor($victim);
+                $damagerSession->addKill();
+                $victimSession->addDeath($damager);
+            }
+            return;
+        }
+        if ($event->getFinalDamage() > $victim->getHealth()) {
+            $session = $this->getSessionFor($victim);
+            $session->addDeath();
+        }
+    }
 
-	/**
-	 * @param PlayerQuitEvent $event
-	 * @priority NORMAL
-	 */
-	public function onPlayerQuit(PlayerQuitEvent $event): void
-	{
-		$player = $event->getPlayer();
-		if (isset($this->sessions[$player->getName()])) unset($this->sessions[$player->getName()]);
-	}
+    /**
+     * @param PlayerQuitEvent $event
+     * @priority NORMAL
+     */
+    public function onPlayerQuit(PlayerQuitEvent $event): void
+    {
+        $player = $event->getPlayer();
+        if (isset($this->sessions[$player->getName()])) unset($this->sessions[$player->getName()]);
+    }
 
-	/**
-	 * @return Main
-	 */
-	public static function getInstance(): Main
-	{
-		return self::$instance;
-	}
+    /**
+     * @return Main
+     */
+    public static function getInstance(): Main
+    {
+        return self::$instance;
+    }
 
-	/**
-	 * @return YamlDataProvider
-	 */
-	public function getYamlProvider(): YamlDataProvider
-	{
-		return $this->yamlProvider;
-	}
+    /**
+     * @return YamlDataProvider
+     */
+    public function getYamlProvider(): YamlDataProvider
+    {
+        return $this->yamlProvider;
+    }
 
-	/**
-	 * @return LeaderboardManager
-	 */
-	public function getLeaderboardManager(): LeaderboardManager
-	{
-		return $this->leaderboardManager;
-	}
+    /**
+     * @return LeaderboardManager
+     */
+    public function getLeaderboardManager(): LeaderboardManager
+    {
+        return $this->leaderboardManager;
+    }
 }
 
