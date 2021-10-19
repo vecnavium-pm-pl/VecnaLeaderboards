aef5579f8cba6335ebc166bca1742d7e68c7b4c3
Vecnavium <83182010+Vecnavium@users.noreply.github.com>
2021-10-18 19:39:09 -0700

    No longer needed

--- src/Vecnavium/VecnaLeaderboards/Main.php
+++ src/Vecnavium/VecnaLeaderboards/Main.php
@@ -39,7 +39,7 @@ class Main extends PluginBase implements Listener
 	public const LEADERBOARD_TYPE_STREAKS = "streaks";
 	public const LEADERBOARD_TYPE_DEATHS = "deaths";
 	public const LEADERBOARD_TYPE_LEVELS = "levels";
-    private bool $isDev = true;
+
 
 	/** @var Main */
 	private static $instance;
