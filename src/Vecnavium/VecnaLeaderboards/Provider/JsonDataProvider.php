6ac6efd6cd8ea58198ba9ddbca7abc14faa5fdb5
Vecnavium <83182010+Vecnavium@users.noreply.github.com>
2022-03-14 13:53:49 -0400

    Bumped config version number

--- src/Vecnavium/VecnaLeaderboards/Provider/JsonDataProvider.php
+++ src/Vecnavium/VecnaLeaderboards/Provider/JsonDataProvider.php
@@ -35,7 +35,7 @@ class JsonDataProvider
 
 	private function init(){
 		$config = $this->plugin->getConfig();
-		if (!$config->exists("CONFIG_VERSION") || $config->get("CONFIG_VERSION") != 3){
+		if (!$config->exists("CONFIG_VERSION") || $config->get("CONFIG_VERSION") != 4){
 			$this->plugin->getLogger()->log(LogLevel::WARNING,"Your config version is outdated. Please delete the config.yml file and restart the server.");
 		}
 		$this->plugin->saveDefaultConfig();
